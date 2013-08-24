b418cf1a46b34d17f1328a93c3a649eb1db11c3c
sweety <smehta1289@gmail.com>
2013-08-24 23:27:40 +0530

    ajax not showing expected response

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,102 @@
+class ProjectsController < ApplicationController
+  # GET /projects
+  # GET /projects.json
+  def index
+    @projects = Project.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @projects }
+    end
+  end
+
+  # GET /projects/1
+  # GET /projects/1.json
+  def show
+    @project = Project.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @project }
+    end
+  end
+
+  # GET /projects/new
+  # GET /projects/new.json
+  def new
+    @project = Project.new
+    # render :js => "alert('Hello Rails!');"
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @project }
+      format.js
+    end
+    # render :inline => "hiii its inline"
+    # head :bad_request
+    # head :created, :location => new_project_path
+
+  end
+
+  # GET /projects/1/edit
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  # POST /projects
+  # POST /projects.json
+  def create
+    @project = Project.new(params[:project])
+
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to @project, notice: 'Project was successfully created.' }
+        format.json { render json: @project, status: :created, location: @project }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /projects/1
+  # PUT /projects/1.json
+  def update
+    @project = Project.find(params[:id])
+
+    respond_to do |format|
+      if @project.update_attributes(params[:project])
+        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /projects/1
+  # DELETE /projects/1.json
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+
+    respond_to do |format|
+      format.html { redirect_to projects_url }
+      format.json { head :no_content }
+    end
+  end
+
+  def ajaxaction
+    # render :text => "alert('hello')"
+    # respond_to do |format|
+    #   format.js{ render action: "alert" }
+    # end
+    # render :nothing => true
+    # render "ajaxaction"
+    # render :ajaxaction
+    # render :action => "ajaxaction"
+    # render :text => "ok"
+    render :js => "alert('Hello Rails!');"
+ end
+ 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ajaxdemo::Application.config.secret_token = '8be49017675e6662571309b742b315424f443fa0af5b9f3e8ed6cf6af58cefa3fec120f9ec07fb1cfbd7128f1866ae8865ac2549191acca43df8fd0664a033ba'
